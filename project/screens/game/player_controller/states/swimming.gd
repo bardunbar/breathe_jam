ffe5a59ceee97112652c8ce12a52c6a316cb556d
bardunbar <61152068+bardunbar@users.noreply.github.com>
2026-04-20 21:16:56 +0100

    Removing camera animations from the player animation node, added a new camera animation node

--- project/screens/game/player_controller/states/swimming.gd
+++ project/screens/game/player_controller/states/swimming.gd
@@ -7,26 +7,26 @@ var current_side : String = "Left"
 func enter(previous_state_path: String, data: Dictionary = {}) -> void:
 	animation_player.animation_finished.connect(_animation_finished)
 		
-	animation_player.play("StrokeRight")
-	animation_player.queue("StrokeLeft")
-	animation_player.queue("StrokeRight")
-	animation_player.queue("BreatheRight")
-	animation_player.queue("StrokeLeft")
-	animation_player.queue("StrokeRight")
-	animation_player.queue("StrokeLeft")
-	animation_player.queue("BreatheLeft")
+	#animation_player.play("StrokeRight")
+	#animation_player.queue("StrokeLeft")
+	#animation_player.queue("StrokeRight")
+	#animation_player.queue("BreatheRight")
+	#animation_player.queue("StrokeLeft")
+	#animation_player.queue("StrokeRight")
+	#animation_player.queue("StrokeLeft")
+	#animation_player.queue("BreatheLeft")
 	#
 	
-	#if previous_state_path == "Breathing" and data["Direction"] != null:
-		#if data["Direction"] == "Left":
-			#animation_player.play("StrokeLeft")
-		#else:
-			#animation_player.play("StrokeRight")
-	#else:	
-		#if current_side == "Left":
-			#animation_player.play("StrokeLeft")
-		#else:
-			#animation_player.play("StrokeRight")
+	if previous_state_path == "Breathing" and data["Direction"] != null:
+		if data["Direction"] == "Left":
+			animation_player.play("StrokeLeft")
+		else:
+			animation_player.play("StrokeRight")
+	else:	
+		if current_side == "Left":
+			animation_player.play("StrokeLeft")
+		else:
+			animation_player.play("StrokeRight")
 		
 
 	
