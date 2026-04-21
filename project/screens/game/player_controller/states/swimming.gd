b17cb74b79a5016deb47d2b30ba5f3dbb21282d9
bardunbar <61152068+bardunbar@users.noreply.github.com>
2026-04-21 16:02:12 +0100

    Add basic ready / pull swim controls

--- project/screens/game/player_controller/states/swimming.gd
+++ project/screens/game/player_controller/states/swimming.gd
@@ -4,56 +4,40 @@ extends State
 @onready var camera_animation : AnimationPlayer = %CameraAnimations
 
 var current_side : String = "Left"
-var breath_count : int = 3
+var left_pull_ready : bool = false
+var right_pull_ready : bool = false
 
-func enter(previous_state_path: String, data: Dictionary = {}) -> void:
+func enter(_previous_state_path: String, _data: Dictionary = {}) -> void:
 	animation_player.animation_finished.connect(_animation_finished)
-	#camera_animation.speed_scale = 0.9
 	
-	#animation_player.play("StrokeRight")
-	#animation_player.queue("StrokeLeft")
-	#animation_player.queue("StrokeRight")
-	#animation_player.queue("BreatheRight")
-	#animation_player.queue("StrokeLeft")
-	#animation_player.queue("StrokeRight")
-	#animation_player.queue("StrokeLeft")
-	#animation_player.queue("BreatheLeft")
-	#
-	
-	if previous_state_path == "Breathing" and data["Direction"] != null:
-		if data["Direction"] == "Left":
-			animation_player.play("StrokeLeft")
-		else:
-			animation_player.play("StrokeRight")
-	else:	
-		if current_side == "Left":
-			animation_player.play("StrokeLeft")
-		else:
-			animation_player.play("StrokeRight")
-		
-
+	animation_player.play("ReadyLeft")
 	
 func exit() -> void:
 	animation_player.animation_finished.disconnect(_animation_finished)
 
 
 func _animation_finished(anim_name: String) -> void:
-	
-	if anim_name == "StrokeLeft":
-		animation_player.play("StrokeRight")
-		current_side = "Right"
-	elif anim_name == "StrokeRight":
-		animation_player.play("StrokeLeft")
-		current_side = "Left"
+	if anim_name == "ReadyLeft":
+		left_pull_ready = true
+	elif anim_name == "ReadyRight":
+		right_pull_ready = true
 		
+
+func trigger_breath(_side: String) -> void:
+	pass
+
+func handle_input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_stroke"):
+		if left_pull_ready or (animation_player.current_animation == "ReadyLeft" and animation_player.current_animation_length - animation_player.current_animation_position < 0.1):
+			left_pull_ready = false
+			animation_player.play("PullLeft")
+			animation_player.queue("ReadyRight")
 			
+	elif event.is_action_pressed("right_stroke"):
+		if right_pull_ready or (animation_player.current_animation == "ReadyRight" and animation_player.current_animation_length - animation_player.current_animation_position < 0.1):
+			right_pull_ready = false
+			animation_player.play("PullRight")
+			animation_player.queue("ReadyLeft")
 			
-func trigger_breath(side: String) -> void:
-	breath_count -= 1
-	
-	if breath_count <= 0:
-		breath_count = 3
-		if side == "Left":
-			camera_animation.play("BreatheLeft")
-		elif side == "Right":
-			camera_animation.play("BreatheRight")
+	elif event.is_action_pressed("breathe"):
+		print("Breathe")
