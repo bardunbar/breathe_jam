b745a5f362f527f16097a967b010a6dbf2af5d03
bardunbar <61152068+bardunbar@users.noreply.github.com>
2026-04-21 17:10:05 +0100

    Added breathing animation

--- project/screens/game/player_controller/states/swimming.gd
+++ project/screens/game/player_controller/states/swimming.gd
@@ -6,6 +6,7 @@ extends State
 var current_side : String = "Left"
 var left_pull_ready : bool = false
 var right_pull_ready : bool = false
+var breathing : bool = false
 
 func enter(_previous_state_path: String, _data: Dictionary = {}) -> void:
 	animation_player.animation_finished.connect(_animation_finished)
@@ -30,14 +31,27 @@ func handle_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_stroke"):
 		if left_pull_ready or (animation_player.current_animation == "ReadyLeft" and animation_player.current_animation_length - animation_player.current_animation_position < 0.1):
 			left_pull_ready = false
+			current_side = "Right"
 			animation_player.play("PullLeft")
 			animation_player.queue("ReadyRight")
+			if breathing:
+				camera_animation.play_backwards("BreatheRight")
+				breathing = false
 			
 	elif event.is_action_pressed("right_stroke"):
 		if right_pull_ready or (animation_player.current_animation == "ReadyRight" and animation_player.current_animation_length - animation_player.current_animation_position < 0.1):
 			right_pull_ready = false
+			current_side = "Left"
 			animation_player.play("PullRight")
 			animation_player.queue("ReadyLeft")
+			if breathing:
+				camera_animation.play_backwards("BreatheLeft")
+				breathing = false
 			
 	elif event.is_action_pressed("breathe"):
-		print("Breathe")
+		if not breathing:
+			breathing = true
+			if current_side == "Left":
+				camera_animation.play("BreatheRight")
+			else:
+				camera_animation.play("BreatheLeft")
